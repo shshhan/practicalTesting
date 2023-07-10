a286d7da6ebfee2c9265550eae0c61aae7df5a3c
shshhan <seunghyeonhan95@gmail.com>
2023-07-11 01:47:11 +0900

    business layer 테스트 - Order

---
+++ src/main/java/sample/cafekiosk/spring/api/domain/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package sample.cafekiosk.spring.api.domain.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sample.cafekiosk.spring.domain.order.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
